60552ce2ed868f08e1b88a408382c13bd75506e4
Michael Scocco <scocco.michael87@gmail.com>
2021-05-18 16:00:48 +0200

    provo a fare sta roba

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let app = new Vue({
+    el: '#app',
+    data:{
+        immagini: [
+            'https://scitechdaily.com/images/Mars-Red-Planet-Highlights-scaled.jpg',
+            'https://media.newyorker.com/photos/60994dabccd28612d3013cec/master/pass/Mann-SpaceEthics.jpg',
+            'https://cdn.britannica.com/24/73524-050-3C2F3432/result-seasons-Mars-planet-inclination-plane-cap.jpg',
+            'https://cdn.mos.cms.futurecdn.net/iWG5zg5Mpt8AvZqc4vYa4K.jpg'
+        ]
+    },
+})
